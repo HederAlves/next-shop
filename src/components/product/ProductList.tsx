8a9e3d4d6d8c209ac046509a4c12d77bc2d4bfac
hederalves <alvesheder92@gmail>
2025-04-01 23:51:46 -0300

    feat: remove comments

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -7,25 +7,19 @@ import Pagination from '../layout/Pagination';
 import SelectQuantity from '../layout/SelectQuantity';
 
 const ProductList = () => {
-    // Estado para controlar o número de produtos a serem exibidos por página
-    const [itemsToShow, setItemsToShow] = useState<number>(8);  // Valor inicial 8
-    const [currentPage, setCurrentPage] = useState<number>(1); // Página inicial é 1
+    const [itemsToShow, setItemsToShow] = useState<number>(8);
+    const [currentPage, setCurrentPage] = useState<number>(1);
 
-    // Função para manipular a seleção de quantidade de itens
     const handleSelect = (value: number) => {
         setItemsToShow(value);
-        setCurrentPage(1);  // Resetar para a primeira página sempre que a quantidade de itens mudar
+        setCurrentPage(1);
     };
 
-    // Calcular os índices de início e fim para a exibição dos produtos
     const indexOfLastProduct = currentPage * itemsToShow;
     const indexOfFirstProduct = indexOfLastProduct - itemsToShow;
     const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
-
-    // Total de páginas, baseado na quantidade total de produtos e na quantidade de itens por página
     const totalPages = Math.ceil(products.length / itemsToShow);
 
-    // Função para alterar a página
     const handlePageChange = (page: number) => {
         if (page >= 1 && page <= totalPages) {
             setCurrentPage(page);
