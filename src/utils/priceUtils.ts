ba2b48b135a8e7f65cee4912872900754838d0fb
Heder Alves <alvesheder92@gmail.com>
2025-04-08 21:55:21 -0300

    Revert "refactor: use button component"

--- src/utils/priceUtils.ts
+++
@@ -1,8 +0,0 @@
-export const calculateDiscountedPrice = (price: number, discount: number | null) => {
-    const hasDiscount = discount !== null && discount > 0;
-    const originalPrice = price;
-    const finalPrice = hasDiscount ? price * (1 - discount / 100) : price;
-    const discountPercent = hasDiscount ? discount : 0;
-
-    return { hasDiscount, originalPrice, finalPrice: finalPrice, discountPercent };
-};
