3c9d9a432f1ff1b5b82161d78e1c0c28ec1f0783
hederalves <alvesheder92@gmail>
2025-04-15 18:10:43 -0300

    refactor: remove suspense from pages

--- src/app/(home)/[id]/page.tsx
+++ src/app/(home)/[id]/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from 'react';
 import Product from '@/components/pages/Product';
 
 export default function ProductPage() {
     return (
-        <Suspense fallback={<div>Carregando produto...</div>}>
-            <Product />
-        </Suspense>
+        <Product />
     );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from 'react';
 import Home from '@/components/pages/Home';
 
 export default function HomePage() {
     return (
-        <Suspense fallback={<div>Carregando...</div>}>
-            <Home />
-        </Suspense>
+        <Home />
     );
 }
