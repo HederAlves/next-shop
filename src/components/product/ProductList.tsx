059fe36e380b6c725fb26987bd52db0cc71302e9
hederalves <alvesheder92@gmail>
2025-04-15 01:56:59 -0300

    refactor: isolate the responsibility of the ProductList component

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,40 +1,33 @@
 'use client'
 
-import { useState } from 'react';
-import products from '../../data/products.json';
-import ProductCard from './ProductCard';
-import Pagination from '../layout/Pagination';
-import SelectQuantity from '../layout/SelectQuantity';
+import ProductCard from '@/components/product/ProductCard';
+import Pagination from '@/components/ui/navegation/Pagination';
 
-const ProductList = () => {
-    const [itemsToShow, setItemsToShow] = useState<number>(8);
-    const [currentPage, setCurrentPage] = useState<number>(1);
+interface ProductListProps {
+    products: any[];
+    currentPage: number;
+    totalPages: number;
+    itemsPerPage: number;
+    totalItems: number;
+    indexOfFirstProduct: number;
+    indexOfLastProduct: number;
+    onPageChange: (page: number) => void;
+}
 
-    const handleSelect = (value: number) => {
-        setItemsToShow(value);
-        setCurrentPage(1);
-    };
-
-    const indexOfLastProduct = currentPage * itemsToShow;
-    const indexOfFirstProduct = indexOfLastProduct - itemsToShow;
-    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
-    const totalPages = Math.ceil(products.length / itemsToShow);
-
-    const handlePageChange = (page: number) => {
-        if (page >= 1 && page <= totalPages) {
-            setCurrentPage(page);
-        }
-    };
-
+const ProductList = ({
+    products,
+    currentPage,
+    totalPages,
+    itemsPerPage,
+    totalItems,
+    indexOfFirstProduct,
+    indexOfLastProduct,
+    onPageChange,
+}: ProductListProps) => {
     return (
-        <div className="px-4 pt-6">
-            <SelectQuantity
-                options={[8, 16, 24]}
-                onSelect={handleSelect}
-            />
-
+        <div className="px-4 space-y-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
-                {currentProducts.map((product) => (
+                {products.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
             </div>
@@ -42,8 +35,11 @@ const ProductList = () => {
             <Pagination
                 currentPage={currentPage}
                 totalPages={totalPages}
-                itemsPerPage={itemsToShow}
-                onPageChange={handlePageChange}
+                itemsPerPage={itemsPerPage}
+                totalItems={totalItems}
+                indexOfFirstProduct={indexOfFirstProduct}
+                indexOfLastProduct={indexOfLastProduct}
+                onPageChange={onPageChange}
             />
         </div>
     );
